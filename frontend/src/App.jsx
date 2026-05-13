678f2c16796244b38c19644aa57530a25d12d8ac
Wissam KHANDJI <khandji.w@gmail.com>
2026-05-13 21:43:18 +0200

    initialisation du projet React avec Vite et Tailwind 4

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white underline">
+        M-Motors est prêt avec Tailwind !
+      </h1>
+    </div>
+  )
+}
+export default App
